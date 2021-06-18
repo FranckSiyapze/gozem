f0bc26fc6a7d7577dad694157b14c7855d5a0d15
unknown <sfranckdabryn@gmail.com>
2021-06-18 09:42:14 +0100

    load all css file essaie 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, AfterViewInit, AfterViewChecked,AfterContentChecked  } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterContentChecked{
   title = 'gozem';
+  constructor(private renderer: Renderer2) {}
+  ngAfterContentChecked(){
+    console.log("after content checked");
+    let loader = this.renderer.selectRootElement('.saving');
+    this.renderer.setStyle(loader, 'display', 'none');
+  }
 }
